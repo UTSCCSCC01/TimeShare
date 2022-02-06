25457cc166cdd5602449155f6e076f5dca9534d7
dxnny8 <danyal.ilyas@mail.utoronto.ca>
2022-02-06 15:15:51 -0500

    finished user stories

--- frontend/src/features/timetable/Timetable.js
+++ frontend/src/features/timetable/Timetable.js
@@ -3,19 +3,34 @@
 // import styles from './Timetable.module.css';
 
 class Timetable {
-    constructor(name, day, start_time) {
+    constructor() {
         this.courses = [];
     }
 
-    add_course(course_to_add) {
+    add_course(course_name) {
         // Add course to this timetable
-        this.courses.push(course_to_add);
+        // console.log("entered!");
+        for (let i = 0; i < all_courses_object.length; i++) {
+            // console.log("a");
+            if (course_name.localeCompare(all_courses_object[i].course_name) == 0) {
+                console.log("added!");
+                this.courses.push(all_courses_object[i]);
+            }
+        }
     }
 
-    remove_course(course_to_add) {
+    remove_course(course_name) {
         // Add course to this timetable
-        const index = this.courses.indexOf(course_to_add);
-        this.courses.splice(index, 1);
+        
+        for (let i = 0; i < this.courses.length; i++) {
+            if (course_name.localeCompare(this.courses[i].course_name) == 0) {
+                console.log("Removed!");
+                this.courses.splice(i, 1);
+                console.log(this.courses);
+            }
+        }
+
+       
     }
 }
 
@@ -39,10 +54,13 @@ class Lecture extends Course {
 }
 
 var fs = require("fs");
+const { ConnectionCheckOutStartedEvent } = require("mongodb");
 var text = fs.readFileSync("./courses_sample.txt") + '';
 var all_courses = text.split("\n");
 var all_courses_object = [];
 var temp;
+var timetable = new Timetable();
+
 
 function createCourseObjects() {
     for (let i = 0; i < all_courses.length; i++) {
@@ -54,14 +72,24 @@ function createCourseObjects() {
     }
 }
 
-function displayTimetable() {
-    console.log("-------------- Timetable --------------");
+function displayCourseSelection() {
+    console.log("--------- Course Selection ---------");
     for (let i = 0; i < all_courses_object.length; i++) {
          //console.log('Course: ${all_courses_object[i].name}, Start time: ${all_courses_object[i].start_time}, End time: ${all_courses_object[i].end_time}, Day: ${convert_day(all_courses_object[i].day}'); 
         console.log("Course: %s, Start time: %s, End time: %s, Day: %s", all_courses_object[i].course_name,
          all_courses_object[i].start_time, all_courses_object[i].end_time, convert_day(all_courses_object[i].day));
     }
 }
+
+function displayMyTimetable() {
+    console.log("--------- Timetable ---------");
+    for (let i = 0; i < timetable.courses.length; i++) {
+         //console.log('Course: ${all_courses_object[i].name}, Start time: ${all_courses_object[i].start_time}, End time: ${all_courses_object[i].end_time}, Day: ${convert_day(all_courses_object[i].day}'); 
+        console.log("Course: %s, Start time: %s, End time: %s, Day: %s", timetable.courses[i].course_name,
+        timetable.courses[i].start_time, timetable.courses[i].end_time, convert_day(timetable.courses[i].day));
+    }
+}
+
 
 function convert_day(day_num) {
     if (day_num == 0) {
@@ -86,14 +114,34 @@ console.log("Hi");
 
 
 
-// process.stdin.on('data', function(data) {
-//     temp = data.toString().split(" ");
-//     console.log(temp[0]);
-//     console.log(all_courses_object[0]);
+process.stdin.on('data', function(data) {
+    temp = data.toString().split(" ");
+    console.log(temp[0]);
 
-// });
+    
+    temp[0] = temp[0].replace(/(\r\n|\n|\r)/gm,"");
+    if (temp[0].localeCompare("add") == 0) {
+        temp[1] = temp[1].replace(/(\r\n|\n|\r)/gm,"");
+        console.log(temp[1]);
+        timetable.add_course(temp[1]);
+    } else if (temp[0].localeCompare("courses") == 0) {
+        console.log("ENTERED!");
+        displayCourseSelection();
+    } else if (temp[0].localeCompare("timetable") == 0) {
+        displayMyTimetable();
+    } else if (temp[0].localeCompare("remove") == 0) {
+        temp[1] = temp[1].replace(/(\r\n|\n|\r)/gm,"");
+        timetable.remove_course(temp[1]);
+    }
 
-displayTimetable();
+    // Add
+    // display
+    // remove
+    
+
+});
+
+
 
 
 
