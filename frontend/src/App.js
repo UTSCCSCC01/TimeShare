10f96e9e6316fb3eea4196463b56334c673d4f36
visanaxel <axel.visan@mail.utoronto.ca>
2022-02-06 12:18:03 -0500

    Created timetable CSS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import { Timetable } from './features/timetable/Timetable';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
+        {/* <Counter /> */}
+        <Timetable/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ frontend/src/features/timetable/Timetable.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import styles from './Timetable.module.css';
+
+
+export function Timetable() {
+
+
+    return (
+        <p>Test</p>
+    )
+}
