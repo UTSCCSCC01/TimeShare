3dc527d7e53e7f6d994d5d348fc1831a063fd616
visanaxel <axel.visan@mail.utoronto.ca>
2022-02-08 20:34:03 -0500

    Updated server which allows POST requests

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,20 @@
+// get driver connection
+const dbo = require("./db/conn");
+
+
 const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 app.use(cors());
 app.use(express.json());
+
 app.use(require("./routes/basic"));
-// get driver connection
-const dbo = require("./db/conn");
+app.use(require("./routes/createTimetable"));
+// app.use(require("./routes/compareTimetable"));
+
+
  
 app.listen(port, () => {
   // perform a database connection when server starts
@@ -15,5 +22,5 @@ app.listen(port, () => {
     if (err) console.error(err);
  
   });
-  console.log(`Server is running on port: ${port}`);
+  console.log('Server is running on port: ${port}');
 });
