d69402ed3dca18a73b4e103c4595374bdff34c58
visanaxel <axel.visan@mail.utoronto.ca>
2022-02-08 19:42:29 -0500

    Moved models to backend

---
+++ backend/models/Course.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const CourseSchema = mongoose.Schema({
+    course_name: {
+        type: String,
+        unique: false,
+        required: [true, "can't be blank"],
+        
+    },
+    course_id: {
+        type: String,
+        lowercase: false,
+        unique: true, // Ex CSC301H5
+        required: [true, "can't be blank"],
+        // double check later on
+        // match: [/^[A-Z]{3}([1-4]|[A-D])\d{2}(H|Y)(1|3|5)(F|S)$/, 'is invalid'], // Finish later
+    }, 
+    description: {
+        type: String,       
+        unique: false,
+        required: [true, "can't be blank"],
+    },
+    lectures: {
+        array_of_lectures: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Lecture' }],
+    },
+
+    tutorials: {
+        array_of_tutorials: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Tutorial' }],
+    },
+    
+    
+});
+
+mongoose.model('Course', CourseSchema);
---
+++ backend/models/Lecture.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const LectureSchema = mongoose.Schema({
+    lecture_id: {
+        type: String,
+        lowercase: true,
+        required: [true, "can't be blank"], 
+        match: [/^[a-zA-Z0-9]+$/, 'is invalid'],
+        unique: true,
+        index: true,
+    },
+    start_time: {
+        type: Number, 
+        unique: false, 
+        required: [true, "can't be blank"], 
+        match: [/0[0-9]|1[0-9]|2[0-3]]/, 'is invalid'], index: true},
+    end_time: {
+        type: Number, 
+        unique: false, 
+        required: [true, "can't be blank"], 
+        match:[/0[0-9]|1[0-9]|2[0-3]]/, 'is invalid'], index: true},
+    day: {
+        type: String, 
+        lowercase: true, 
+        unique: false, 
+        required: [true, "can't be blank"], 
+        match: [/(Monday|Tuesday|Wednesday|Thursday|Friday)/, 'is invalid'], index: true} //fix
+});
+
+mongoose.model('Lecture', LectureSchema);
