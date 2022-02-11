59f956011570fb3bbc14bc07440bf8a415e0ead2
Burhan Mirza <burhan.mirza@audiopticsmedical.com>
2022-02-10 20:06:50 -0500

    created login backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const {
     createUser,
-    getUsers
+    getUsers,
+    loginUser,
 } = require("../controllers/userController")
 
 router.get('/', getUsers);
 router.post('/create', createUser);
+router.post('/login', loginUser);
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,6 @@ const connectDB = require('./db/conn')
 const dotenv = require("dotenv").config({ path: "./config.env" })
 const port = process.env.PORT || 5000
 
-
 connectDB()
 
 const app = express()
