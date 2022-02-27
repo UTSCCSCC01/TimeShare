e815eaa540ca361e1935e0b9bccfb106a132018c
Saiem Irfan <saiem2000@hotmail.com>
2022-02-27 02:25:06 -0500

    WAV-45 Adds update profile endpoint and documentation

--- backend/controllers/profileController.js
+++ backend/controllers/profileController.js
@@ -8,15 +8,16 @@ require('../models/User')
 var User = mongoose.model('User');
 
 const createProfile = asyncHandler(async (req, res, next) => {
-    const { uid, first_name, last_name, program, year_of_study, phone, desc } = req.body
+    const { uid, first_name = "", last_name = "", program = "", year_of_study = "", phone = "", desc = "" } = req.body
     let profile = Profile({user: uid, first_name, last_name, program, year_of_study, phone, description: desc})
-    e = profile.validateSync()
+    e = await profile.validateSync()
     let errors = {}
 
     correspondingUser = await User.findOne({_id: uid})
     if(!correspondingUser){
         errors["user"] = ["User with given ID does not exist!"]
         res.status(404).json({validationErrors: errors})
+        return
     }
 
 
@@ -38,6 +39,10 @@ const createProfile = asyncHandler(async (req, res, next) => {
     if(dupUser){
         if(errors["user"]){
             errors["user"].push("User can only have one profile!")
+        }
+        else{
+            errors["user"] = ["User can only have one profile!"]
+        }
     }
 
     if(Object.keys(errors).length > 0){
@@ -57,8 +62,8 @@ const getProfiles = asyncHandler(async (req, res, next) => {
         res.status(200).json(profiles)
      }).catch(function (err){
         next(err)
-     })}
-)
+     })
+    })
 
 const deleteProfiles = asyncHandler( async (req, res, next) => {
     let query = req.params.profileId ? Profile.deleteOne({_id: req.params.profileId}) : Profile.deleteMany() 
@@ -70,8 +75,48 @@ const deleteProfiles = asyncHandler( async (req, res, next) => {
     })
 })
 
+const updateProfile = asyncHandler( async (req, res, next) => {
+    const { pid, first_name, last_name, program, year_of_study, phone, desc } = req.body
+    let profile = await Profile.findOne({_id: pid})
+
+    let errors = {}
+    if(!profile){
+        errors["profile"] = ["Profile with given ID does not exist"]
+        res.status(404).json(errors)
+        return
+    }
+
+    profile.first_name = first_name
+    profile.last_name = last_name
+    profile.program = program
+    profile.year_of_study = year_of_study
+    profile.phone = phone
+    profile.description = desc
+
+    let e = await profile.validateSync()
+
+    if(e){
+        Object.keys(e.errors).map((key) => {
+            if(!errors[key]){
+                errors[key] = []
+            }
+            errors[key].push(e.errors[key].message)
+        })
+    }
+
+    if(Object.keys(errors).length > 0){
+        res.status(400).json({validationErrors: errors})
+    }
+    else{
+        profile = await profile.save()
+        res.status(201).json(profile)
+    }
+    
+})
+
 module.exports = {
     createProfile,
     getProfiles,
-    deleteProfiles
+    deleteProfiles,
+    updateProfile
 }
