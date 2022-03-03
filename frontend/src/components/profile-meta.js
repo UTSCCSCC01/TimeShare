925c543f711e54647c6009d6f7af6b375d657a8c
Saiem Irfan <saiem2000@hotmail.com>
2022-03-02 23:54:44 -0500

    WAV-46 Adds profile metadata views and grid layout for self profile view

---
+++ frontend/src/components/profile-meta.js
@@ -0,0 +1,82 @@
+import React from "react"
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import { Box } from "@mui/system";
+import { Stack } from "@mui/material";
+import { Button } from "@mui/material";
+
+class ProfileMetaText extends React.Component {
+    render () {
+        const name_str = `Name: ${this.props.first_name} ${this.props.last_name}`
+        const desc_str = `Description: ${this.props.description}`
+        const year_str = `Year: ${this.props.year}`
+        const program_str = `Program: ${this.props.program}`
+
+        return (
+            <List>
+                <ListItem>
+                    <ListItemText
+                    sx={{textAlign: "center"}}
+                    primary={name_str}
+                    />
+                </ListItem>
+                <ListItem>
+                    <ListItemText
+                    sx={{textAlign: "center"}}
+                    primary={desc_str}
+                    />
+                </ListItem>
+                <ListItem>
+                    <ListItemText
+                    sx={{textAlign: "center"}}
+                    primary={year_str}
+                    />
+                </ListItem>
+                <ListItem>
+                    <ListItemText
+                    sx={{textAlign: "center"}}
+                    primary={program_str}
+                    />
+                </ListItem>
+            </List>
+        )
+    }
+}
+
+class ProfileMeta extends React.Component {
+    render () {
+        return(
+            <Stack spacing={2}>
+                <Box sx={{width: this.props.maxWidth, textAlign: "center"}}><b>{this.props.username}</b></Box>
+                <Box sx={{
+                    maxWidth: this.props.maxWidth,
+                    maxHeight: 300,
+                    textAlign: "center",
+                }}>
+                    <img style={{maxWidth: "inherit", objectFit: "contain", maxHeight: "inherit"}} src="https://images.unsplash.com/photo-1554080353-a576cf803bda?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cGhvdG98ZW58MHx8MHx8&w=1000&q=80"></img>
+                </Box>
+                <Box sx={{
+                    maxWidth: this.props.maxWidth,
+                    textAlign: "center",
+                    justifyContent: "center"
+                }}><ProfileMetaText first_name={this.props.first_name} 
+                    last_name={this.props.last_name}
+                    year={this.props.year}
+                    program={this.props.program}
+                    description={this.props.description}/>
+                </Box>
+                <Box sx={{
+                    maxWidth: this.props.maxWidth,
+                    textAlign: "center",
+                }}>
+                    <Button sx={{maxWidth: this.props.maxWidth / 2}} variant="contained">Update Profile</Button>
+                </Box>
+            </Stack>
+        )
+    }
+}
+
+export {
+    ProfileMeta
+}
--- frontend/src/pages/ViewProfile.jsx
+++ frontend/src/pages/ViewProfile.jsx
@@ -1,20 +1,18 @@
 import React from "react";
-import { Grid } from "@mui/material";
-
+import { Box, Grid } from "@mui/material";
 import { Arrow } from "../components/arrow-component";
 import { PostList } from "../components/post-show-list";
-import { maxWidth } from "@mui/system";
-
+import { ProfileMeta } from "../components/profile-meta";
 class ProfileView extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            "first_name": "",
-            "last_name": "",
-            "program": "",
-            "year_of_study": "",
+            "first_name": "bobby",
+            "last_name": "green",
+            "program": "cs",
+            "year_of_study": "5",
             "phone": "", 
-            "description": "",
+            "description": "Hi, I'm bobby. You will often see me about because i'm an active utm student trying to do my best for the environment!",
             "avatar": "",
             "timetables": [],
             "public_groups": [],
@@ -23,6 +21,7 @@ class ProfileView extends React.Component {
             "private_viewable?": false,
             "start_posts": 0,
             "num_posts": 3,
+            "username": "basic_username",
         }
 
         if (this.props.isMe){
@@ -45,17 +44,43 @@ class ProfileView extends React.Component {
     }
 
     async componentDidMount () {
-         let data = {} //backendstuff to get profile
-        let new_data = {}
-        let old_data = {}
-        Object.keys(old_data).forEach(key => {
-            new_data[key] = data[key] || old_data[key]
-        })
-        
-        this.setState({ new_data })
-    }
+        let private_groups = [
+            {
+                "group_image": "https://images.unsplash.com/photo-1506869640319-fe1a24fd76dc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Z3JvdXB8ZW58MHx8MHx8&w=1000&q=80",
+                "group_name": "Private group 1"
+            },
+            {
+                "group_image": "https://images.unsplash.com/photo-1506869640319-fe1a24fd76dc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Z3JvdXB8ZW58MHx8MHx8&w=1000&q=80",
+                "group_name": "Private group 2"
+            }
+        ]
 
-    getViewablePosts() {
+        let public_groups = [
+            {
+                "group_image": "https://images.unsplash.com/photo-1506869640319-fe1a24fd76dc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Z3JvdXB8ZW58MHx8MHx8&w=1000&q=80",
+                "group_name": "Public group 1"
+            },
+            {
+                "group_image": "https://images.unsplash.com/photo-1506869640319-fe1a24fd76dc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Z3JvdXB8ZW58MHx8MHx8&w=1000&q=80",
+                "group_name": "Public group 2"
+            },
+            {
+                "group_image": "https://images.unsplash.com/photo-1506869640319-fe1a24fd76dc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Z3JvdXB8ZW58MHx8MHx8&w=1000&q=80",
+                "group_name": "Public group 3"
+            },
+            {
+                "group_image": "https://images.unsplash.com/photo-1506869640319-fe1a24fd76dc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Z3JvdXB8ZW58MHx8MHx8&w=1000&q=80",
+                "group_name": "Public group 4"
+            },
+            {
+                "group_image": "https://images.unsplash.com/photo-1506869640319-fe1a24fd76dc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Z3JvdXB8ZW58MHx8MHx8&w=1000&q=80",
+                "group_name": "Public group 5"
+            },
+            {
+                "group_image": "https://images.unsplash.com/photo-1506869640319-fe1a24fd76dc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Z3JvdXB8ZW58MHx8MHx8&w=1000&q=80",
+                "group_name": "Public group 6"
+            }
+        ]
         const posts = [
             {
                 "key": "12343qasfa112",
@@ -76,6 +101,17 @@ class ProfileView extends React.Component {
                 "description": "Hi guys, as a cool guy from UTM, I'd love to share my timetable! LMK what you think!"
             } 
         ]
+        let data = {posts, public_groups, private_groups} // usually do backend stuff
+        // let new_data = {}
+        // let old_data = this.state
+        // Object.keys(old_data).forEach(key => {
+        //     new_data[key] = data[key] || old_data[key]
+        // })
+        this.setState({ posts, public_groups, private_groups })
+    }
+
+    getViewablePosts() {
+        const posts = this.state.posts
         const start = this.state.start_posts
         const num_posts = this.state.num_posts
 
@@ -86,18 +122,36 @@ class ProfileView extends React.Component {
         let posts = this.getViewablePosts()
 
         return (
-            <Grid container spacing={1} sx={{maxWidth: 1200}}
-            align-items="center">
-                <Grid item xs>
-                    <Arrow direction="left" onClick={this.leftArrowClick}></Arrow>
+            <div class="profile-view-container">
+                <ProfileMeta
+                    sx={{gridColumnStart: 1,
+                        gridColumnEnd: 1,
+                        gridRowEnd: 4,
+                        gridRowStart: 1}}
+                    maxWidth={300}
+                    first_name={this.state.first_name} 
+                    last_name={this.state.last_name}
+                    year={this.state.year_of_study}
+                    program={this.state.program}
+                    description={this.state.description}
+                    username={this.state.username}/>  
+                <Grid container spacing={1} sx={{maxWidth: 1200,
+                    gridColumnStart: 2,
+                    gridColumnEnd: 5,
+                    gridRowEnd: 3,
+                    gridRowStart: 1}}
+                align-items="center">
+                    <Grid item xs>
+                        <Arrow direction="left" onClick={this.leftArrowClick}></Arrow>
+                    </Grid>
+                    <Grid item xs={11}>
+                        <PostList posts={posts} width={1000}></PostList>
+                    </Grid>
+                    <Grid item xs>
+                        <Arrow direction="right" onClick={this.rightArrowClick}></Arrow>
+                    </Grid>
                 </Grid>
-                <Grid item xs={11}>
-                    <PostList posts={posts} width={1000}></PostList>
-                </Grid>
-                <Grid item xs>
-                    <Arrow direction="right" onClick={this.rightArrowClick}></Arrow>
-                </Grid>
-            </Grid>
+            </div>
         );
     }
 }
