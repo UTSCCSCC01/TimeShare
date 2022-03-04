4e1f380548b93e7905bef8fcef2f5b929e7a8077
dxnny8 <danyal.ilyas@mail.utoronto.ca>
2022-03-04 00:11:49 -0500

    WAV-43/Create_timetable_interface/ added the ui for adding courses to timetable with Brooklyn, Ramzi, Axel

--- frontend/src/pages/CreateTimetable.jsx
+++ frontend/src/pages/CreateTimetable.jsx
@@ -86,7 +86,68 @@ export const CreateTimetable = () => {
 
   return (
 
-   <h1>hi</h1>
+    <><><div><nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">TimeShare</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Feed</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Profile
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="#">edit profile</a></li>
+              <li><a class="dropdown-item" href="#">blah</a></li>
+              <li><hr class="dropdown-divider" /></li>
+              <li><a class="dropdown-item" href="#">blah</a></li>
+            </ul>
+          </li>
+
+        </ul>
+
+      </div>
+    </div>
+  </nav>
+  </div><div class="timetable">    <br /><br />
+
+
+
+      <h1>Timetable Builder</h1>    <br /><br />
+      <link rel="stylesheet" href="../App.css" />
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+      <input type="text" id="myInput" onKeyUp={myFunction} placeholder="Search for a course" />
+
+      <ul id="myUL" style={{ maxHeight: 200, overflow: 'auto' }}>
+        {courses.map((item, index) => {
+          return <li key={index} onClick={(e) => {
+            e.preventDefault();
+            addEvent(index);
+          } }><a href="#">{item.name_time}</a></li>;
+        })}
+      </ul>
+
+      <br /><br />
+
+      <div>
+        <Timetable class="timetable2"
+
+          events={events} />
+      </div>
+      <footer>
+<p class="text-center">Copyright 2022 &copy; TimeShare</p>
+</footer>
+      
+    </div></><div>
+     </div></>
 
   )
 }
