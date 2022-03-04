d4d246f9cdd522050bc4993d1428ade583298b94
visanaxel <axel.visan@mail.utoronto.ca>
2022-03-03 23:53:48 -0500

    WAV-43/Create_timetable_interface/Worked on the timetable creation with Ramzi, Brooklyn and Danyal. Implemented search bar which allows to select courses to add to the timetable.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,15 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import { CreateTimetable } from './pages/CreateTimetable';
 import { App2 } from './pages/CreateTimetable';
-import { CreateUser } from './pages/CreateUser';
+// import { CreateUser } from './pages/CreateUser';
 import { UpdateProfile } from './pages/UpdateProfile';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/createTimetable" element={<App2/>} />
-        <Route path="/createUser" element={<CreateUser/>} />
+        <Route path="/createTimetable" element={<CreateTimetable/>} />
+        {/* <Route path="/createUser" element={<CreateUser/>} /> */}
         <Route path="/updateProfile" element={<UpdateProfile/>} />
       </Routes>
     </Router>
--- frontend/src/pages/CreateTimetable.jsx
+++ frontend/src/pages/CreateTimetable.jsx
@@ -1,89 +1,95 @@
 // import * as React from 'react'
 import Timetable from 'react-timetable-events'
 import ReactDOM from 'react-dom'
+import { makeStyles } from "@material-ui/core/styles";
+import React, { useState } from 'react'
 
-import React, {useState} from 'react'
-import Calendar from 'react-calendar'
-import moment from 'moment'
-import TextField from "@mui/material/TextField";
-// import List from "./Components/List"
-// import Search from './search';
 
-import SearchBar from "material-ui-search-bar";
+// -------
 
-export function App2() {
-  const [dateState, setDateState] = useState(new Date())
-  const changeDate = (e) => {
-    setDateState(e)
+export const CreateTimetable = () => {
+
+  const [search, setSearch] = useState("");
+
+  class Section {
+    constructor(startTime, endTime, day, name, id, type) {
+      this.day = day
+      this.endTime = endTime
+      this.startTime = startTime
+      this.name = name
+      this.id = id
+      this.type = type
+      this.name_time = name + " - " + day.charAt(0).toUpperCase() + day.slice(1) + " " + startTime.toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'}) + " to " + endTime.toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'})
+    }
   }
-  return (
-    <>
-    <p>hi</p>
-    {/* <SearchBar/> */}
-      <CreateTimetable/>
-    <p>Current selected date is <b>{moment(dateState).format('MMMM Do YYYY')}</b></p>
-    </>
-  )
-}
 
-// const SearchBar = () => (
-//   <form action="/" method="get">
-//       <label htmlFor="header-search">
-//           <span className="visually-hidden">Search blog posts</span>
-//       </label>
-//       <input
-//           type="text"
-//           id="header-search"
-//           placeholder="Search a course..."
-//           name="s" 
-//       />
-//       <button type="submit">Search</button>
-//   </form>
-// );
+  // 3 dummy courses: 2 lecs and 1 tut for each
+  // startime                          endtime                         day         name           id    type!=type in events which is always custom
+  let csc420lec9101 = new Section(new Date("2018-02-23T11:00:00"), new Date("2018-02-23T12:00:00"), "thursday", "CSC420 LEC9101", 3, "lecture", "")
+  let csc420lec9102 = new Section(new Date("2018-02-23T13:00:00"), new Date("2018-02-23T15:00:00"), "monday", "CSC420 LEC9102", 4, "lecture")
+  let csc420tut9103 = new Section(new Date("2018-02-23T21:00:00"), new Date("2018-02-23T12:00:00"), "thursday", "CSC420 PRA9101", 5, "tutorial")
+  let sta420lec9101 = new Section(new Date("2018-02-23T09:00:00"), new Date("2018-02-23T11:00:00"), "thursday", "STA420 LEC9101", 6, "lecture")
+  let sta420lec9102 = new Section(new Date("2018-02-23T11:00:00"), new Date("2018-02-23T12:00:00"), "friday", "STA420 LEC9102", 7, "lecture")
+  let sta420tut9103 = new Section(new Date("2018-02-23T17:00:00"), new Date("2018-02-23T19:00:00"), "thursday", "STA420 PRA9101", 8, "tutorial")
+  let sci420lec9101 = new Section(new Date("2018-02-23T20:00:00"), new Date("2018-02-23T22:00:00"), "thursday", "MAT420 LEC9101", 9, "lecture")
+  let sci420lec9102 = new Section(new Date("2018-02-23T10:00:00"), new Date("2018-02-23T12:00:00"), "friday", "MAT420 LEC9102", 10, "lecture")
+  let sci420tut9103 = new Section(new Date("2018-02-23T11:13:00"), new Date("2018-02-23T15:00:00"), "thursday", "MAT420 PRA9101", 11, "tutorial")
+
+  let courses = [csc420lec9101, csc420lec9102, csc420tut9103, sta420lec9101, sta420lec9102, sta420tut9103, sci420lec9101, sci420lec9102, sci420tut9103]
 
 
-
-export const CreateTimetable = () => <Timetable 
-
-  events={{
-    monday: [
-      {
-        id: 1,
-        name: "Custom Event 1",
-        type: "custom",
-        startTime: new Date("2018-02-23T11:00:00"),
-        endTime: new Date("2018-02-23T14:00:00"),
-      },
-    ],
+  const [events, setEvents] = useState({
+    monday: [],
     tuesday: [],
     wednesday: [],
     thursday: [],
-    friday: [
-      {
-        id: 1,
-        name: "Danny",
-        type: "custom",
-        startTime: new Date("2018-02-23T11:00:00"),
-        endTime: new Date("2018-02-23T14:00:00"),
-      }
-    ],
-  }}
-/>
+    friday: [],
+  })
 
-export function Hello () {
+  const addEvent = (index) => {
+    let course = courses[index]
+    let day = course.day
+    console.log(events)
+    let cp = { ...events }
+    cp[day].push({
+      id: course.id,
+      name: course.name,
+      type: "custom",
+      startTime: course.startTime,
+      endTime: course.endTime,
+    })
 
-  ReactDOM.render(
-  <div
-    style={{
-        position: 'absolute', left: '50%', top: '50%',
-        transform: 'translate(-50%, -50%)'
-    }}
-    >
-    Hello, world!
-  </div>,
-  <p>hi</p>
-  );
+    setEvents(cp)
+    //console.log(events)
   }
+
+  function myFunction() {
+    // Declare variables
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName('li');
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      txtValue = a.textContent || a.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
+    }
+  }
+
+
+  return (
+
+   <h1>hi</h1>
+
+  )
+}
 
 
 
