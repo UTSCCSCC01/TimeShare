69e0f65793f96eacaf0cc793aa676655f4e7d88f
Burhan Mirza <burhan.mirza@audiopticsmedical.com>
2022-03-03 22:30:58 -0500

    WAV-29/modifcations to model

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,13 @@ const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./db/conn')
 const dotenv = require("dotenv").config({ path: "./config.env" })
 const port = process.env.PORT || 5000
-
+const cors = require("cors")
 connectDB()
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(cors())
 
 
 app.use("/api/Profiles", require("./routes/profileRoutes"))
