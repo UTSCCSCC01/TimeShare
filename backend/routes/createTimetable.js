fc5c99486a3d36ced3e6f52858041057e03ce147
Ramzi Abu-Zeineh <ramzi.abuzeineh@gmail.com>
2022-03-16 18:08:39 -0400

    WAV-19/Create_View_Post Adds routing to list all courses

--- backend/routes/createTimetable.js
+++ backend/routes/createTimetable.js
@@ -10,5 +10,6 @@ router.get('/getPost', TimetableController.get_post);
 router.post('/addCourse', TimetableController.add_course);
 router.post('/removeCourse', TimetableController.remove_course);
 router.get('/getCourses', TimetableController.get_courses);
+router.get('/getAllCourses', TimetableController.get_courses);
 
 module.exports = router;
