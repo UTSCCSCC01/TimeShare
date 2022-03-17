e6252ba7a723ac2e6d448448d569edf9ad42c177
Brooklyn Guo <brooklyn.guo@mail.utoronto.ca>
2022-03-16 23:35:58 -0400

    pushes changes to display courses to 2 timetables side by side

--- frontend/src/pages/CompareTimetables.jsx
+++ frontend/src/pages/CompareTimetables.jsx
@@ -2,12 +2,11 @@
 import Timetable from 'react-timetable-events'
 import ReactDOM from 'react-dom'
 import { makeStyles } from "@material-ui/core/styles";
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Axios from 'axios';
+import { blue, red } from '@material-ui/core/colors';
 
 export const CompareTimetables = () => {
-
-    const [search, setSearch] = useState("");
   
     class Section {
       constructor(startTime, endTime, day, name, id, type) {
@@ -25,40 +24,47 @@ export const CompareTimetables = () => {
     // startime                          endtime                         day         name           id    type!=type in events which is always custom
     let csc420lec9101 = new Section(new Date("2018-02-23T11:00:00"), new Date("2018-02-23T12:00:00"), "thursday", "CSC420 LEC9101", 3, "lecture", "")
     let csc420lec9102 = new Section(new Date("2018-02-23T13:00:00"), new Date("2018-02-23T15:00:00"), "monday", "CSC420 LEC9102", 4, "lecture")
-    let csc420tut9103 = new Section(new Date("2018-02-23T21:00:00"), new Date("2018-02-23T12:00:00"), "thursday", "CSC420 PRA9101", 5, "tutorial")
+    let csc420tut9101 = new Section(new Date("2018-02-23T21:00:00"), new Date("2018-02-23T12:00:00"), "thursday", "CSC420 PRA9101", 5, "tutorial")
     let sta420lec9101 = new Section(new Date("2018-02-23T09:00:00"), new Date("2018-02-23T11:00:00"), "thursday", "STA420 LEC9101", 6, "lecture")
     let sta420lec9102 = new Section(new Date("2018-02-23T11:00:00"), new Date("2018-02-23T12:00:00"), "friday", "STA420 LEC9102", 7, "lecture")
-    let sta420tut9103 = new Section(new Date("2018-02-23T17:00:00"), new Date("2018-02-23T19:00:00"), "thursday", "STA420 PRA9101", 8, "tutorial")
-    let sci420lec9101 = new Section(new Date("2018-02-23T20:00:00"), new Date("2018-02-23T22:00:00"), "thursday", "MAT420 LEC9101", 9, "lecture")
-    let sci420lec9102 = new Section(new Date("2018-02-23T10:00:00"), new Date("2018-02-23T12:00:00"), "friday", "MAT420 LEC9102", 10, "lecture")
-    let sci420tut9103 = new Section(new Date("2018-02-23T11:13:00"), new Date("2018-02-23T15:00:00"), "thursday", "MAT420 PRA9101", 11, "tutorial")
+    let sta420tut9101 = new Section(new Date("2018-02-23T17:00:00"), new Date("2018-02-23T19:00:00"), "thursday", "STA420 PRA9101", 8, "tutorial")
+    let mat420lec9101 = new Section(new Date("2018-02-23T20:00:00"), new Date("2018-02-23T22:00:00"), "thursday", "MAT420 LEC9101", 9, "lecture")
+    let mat420lec9102 = new Section(new Date("2018-02-23T10:00:00"), new Date("2018-02-23T12:00:00"), "friday", "MAT420 LEC9102", 10, "lecture")
+    let mat420tut9101 = new Section(new Date("2018-02-23T11:13:00"), new Date("2018-02-23T15:00:00"), "thursday", "MAT420 PRA9101", 11, "tutorial")
 
-    Axios.get("http://localhost:5000/api/Timetable/getCourses", {id: 321})
-      .then((res) => {
-
-        console.log("hi")
-      })
-      .catch((error) => {
-        console.log("Error:", error);
-      });
+    let courses1 = [csc420lec9101, csc420tut9101, mat420lec9102, mat420tut9101]
+    let courses2 = [sta420lec9101, sta420tut9101, mat420lec9101, mat420tut9101]
   
-    let courses = [csc420lec9101, csc420lec9102, csc420tut9103, sta420lec9101, sta420lec9102, sta420tut9103, sci420lec9101, sci420lec9102, sci420tut9103]
-  
-  
-    const [events, setEvents] = useState({
+    const [events1, setEvents1] = useState({
       monday: [],
       tuesday: [],
       wednesday: [],
       thursday: [],
       friday: [],
     })
-  
-    const addEvents = (lectures, tutorials) => {
-      for (let i = 0; i < courses.length; i++) {
-        let course = courses[i]
+
+    const [events2, setEvents2] = useState({
+      monday: [],
+      tuesday: [],
+      wednesday: [],
+      thursday: [],
+      friday: [],
+    })
+
+    const addEvents1 = () => {
+      Axios.get("http://localhost:3001/api/Timetable/getCourses", {id: 321})
+      .then((res) => {
+        courses1 = []
+        console.log(res);
+      })
+      .catch((error) => {
+        courses1 = []
+        console.log("Error:", error);
+      });
+      for (let i = 0; i < courses1.length; i++) {
+        let course = courses1[i]
         let day = course.day
-        console.log(events)
-        let cp = { ...events }
+        let cp = { ...events1 }
         cp[day].push({
           id: course.id,
           name: course.name,
@@ -67,47 +73,78 @@ export const CompareTimetables = () => {
           endTime: course.endTime,
         })
     
-        setEvents(cp)
+        setEvents1(cp)
         //console.log(events)
       }
       
     }
   
-    function myFunction() {
-      // Declare variables
-      var input, filter, ul, li, a, i, txtValue;
-      input = document.getElementById('myInput');
-      filter = input.value.toUpperCase();
-      ul = document.getElementById("myUL");
-      li = ul.getElementsByTagName('li');
-  
-      // Loop through all list items, and hide those who don't match the search query
-      for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          li[i].style.display = "";
-        } else {
-          li[i].style.display = "none";
-        }
-      }
-    }
+    const addEvents2 = () => {
+      for (let i = 0; i < courses2.length; i++) {
+        let course = courses2[i]
+        let day = course.day
+        console.log(events2)
+        let cp = { ...events2 }
+        cp[day].push({
+          id: course.id,
+          name: course.name,
+          type: "custom",
+          startTime: course.startTime,
+          endTime: course.endTime,
+        })
     
+        setEvents2(cp)
+        //console.log(events)
+      }
+      
+    }
     
     const styles = {
       splitScreen: {
         display: 'flex',
         flexDirection: 'row'
       },
-      topPane: {
-          width: '50%'
+      leftPane: {
+          // backgroundColor: "#44014C",
+          margin: '5px',
+          width: '50%',
+          height: '100%',
+          // resizeMode: 'contain',
       },
-      bottomPane: {
-          width: '50%'
+      rightPane: {
+          // backgroundColor: "#20c997",
+          margin: '5px',
+          width: '50%',
+          height: '100%',
+          // resizeMode: 'contain',
       },
     }
     
-  
+    // on page load, calls the functions to display courses on each timetable
+    useEffect(() => {
+      addEvents1();
+      addEvents2();
+    }, []);
+
+    // renderEvent(event, defaultAttributes, styles) {
+    //   return (
+    //     <div
+    //       {...defaultAttributes}
+    //       title={event.name}
+    //       key={event.id}
+    //       style={{
+    //         ...defaultAttributes.style,
+    //         backgroundColor: "blue"
+    //       }}
+    //     >
+    //       <span className={styles.event_info}>[ {event.name} ]</span>
+    //       <span className={styles.event_info}>
+    //         {event.startTime.format("HH:mm")} - {event.endTime.format("HH:mm")}
+    //       </span>
+    //     </div>
+    //   );
+    // }
+
     return (
   
       <><><div><nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -150,24 +187,26 @@ export const CompareTimetables = () => {
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
   
         {/* <ul id="myUL" style={{ maxHeight: 200, overflow: 'auto' }}>
-          {courses.map((item, index) => {
-            return <li key={index} onClick={(e) => {
-              e.preventDefault();
-              addEvent(index);
-            } }><a href="#">{item.name_time}</a></li>;
-          })}
+        {courses1.map((item, index) => {
+          return <li key={index} onClick={(e) => {
+            e.preventDefault();
+            addEvents1()
+            addEvents2()
+          } }><a href="#">{item.name_time}</a></li>;
+        })}
         </ul> */}
-  
-        <br /><br />
-  
-        <div className={styles.splitScreen}>
-          <div className={styles.topPane}>
-            <Timetable events={events} />
+        
+        <div style={styles.splitScreen}>
+          <div style={styles.leftPane}>
+            <Timetable id="tt1" events={events1} />
           </div>
-          <div className={styles.bottomPane}>
-            <Timetable  events={events} />
+          <div style={styles.rightPane}>
+            <Timetable id="tt2" events={events2} />
           </div>
         </div>
+
+        <br />
+        <br />
 
 
 
