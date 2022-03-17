6f0ceac6360b37005c575727ae6f1b10389c28b1
visanaxel <axel.visan@mail.utoronto.ca>
2022-03-17 15:49:20 -0400

    WAV-1/Labels_To_Post Added create post interface

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -4,15 +4,19 @@ import TextField from '@material-ui/core/TextField';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import { Button, Paper } from "@material-ui/core";
-import Grid from "@material-ui/core/Grid";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
 import FormControl from "@material-ui/core/FormControl";
-import FormLabel from "@material-ui/core/FormLabel";
-import RadioGroup from "@material-ui/core/RadioGroup";
-import Radio from "@material-ui/core/Radio";
-import Slider from "@material-ui/core/Slider";
 import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
 
+import Divider from '@mui/material/Divider';
+import MenuList from '@mui/material/MenuList';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Typography from '@mui/material/Typography';
+import ContentCut from '@mui/icons-material/ContentCut';
+import ContentCopy from '@mui/icons-material/ContentCopy';
+import ContentPaste from '@mui/icons-material/ContentPaste';
+import Cloud from '@mui/icons-material/Cloud';
 
 export const CreatePost = () => {
 
@@ -70,7 +74,7 @@ export const CreatePost = () => {
             console.log("SHOULD NOT SEE THIS!")
             return
         }
-        
+
         const data = {
             "label": label,
             "name": title,
@@ -79,105 +83,93 @@ export const CreatePost = () => {
         }
 
         Axios.post("http://localhost:5112/api/Timetable/createPost", data)
-        
- 
+
+
     }
-    
-  
-    
-  
+
+
+
+
     return (
         <div>
             <h1>Create Post</h1>
-            <FormControl class="container" onSubmit={(e) => {
-            e.preventDefault();
-          }}
-            >   
-                    
- 
-                    
-                    <TextField label="Post Title" class="input" id="title" variant="filled" 
-                    error={errors.username !== ''} helperText={errors.username}
-                 required onChange={(e) => {
-                    setTitle(e.target.value);
-                  }}/>  
-                  <br></br>
-                  <hr></hr>
 
-  
-                    <label for="description" required> Description </label>  
-                    <TextField class="input" required label="Filled" variant="filled" onChange={(e) => {
-                    setDescription(e.target.value);
-                  }} id="description" type="textarea" name="description"/>
-
-                  <label for="label">Choose a label:</label>
-                    <Select class="input" id="label" name="label" required onChange={(e) => {
-                    setLabel(e.target.value);
-                  }}>
-                    <MenuItem value="1st-year">1st-year</MenuItem>
-                    <MenuItem value="2nd-year">2nd-year</MenuItem>
-                    <MenuItem value="3rd-year">3rd-year</MenuItem>
-                    <MenuItem value="4th-year">4th-year</MenuItem>
-                    </Select>  
-                 <br></br>
-                <Button type="submit" onClick={makePost}>Create Post</Button>  
-  
-            </FormControl>  
 
             <Box
-      component="form"
-      sx={{
-        '& .MuiTextField-root': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
+                class="container"
+                component="form"
+                sx={{
+                    '& .MuiTextField-root': { m: 5, width: '25ch' }
 
-      <div>
-        <TextField 
-            label="Post Title" 
-            id="title" 
-            variant="filled" 
-            error={errors.username !== ''} 
-            helperText={errors.username}
-            required 
-            onChange={(e) => {
-            setTitle(e.target.value);
-                  }}/>  
-        <TextField 
-            required 
-            label="Description" 
-            variant="filled" 
-            onChange={(e) => {
-                    setDescription(e.target.value);
-                  }} 
-            id="description" 
-            multiline
-            rows={8}
-            name="description"/>
-        <Select 
-          variant="filled" 
-          class="input" 
-          id="label" 
-          name="label" 
-          required 
-          onChange={(e) => {
-                    setLabel(e.target.value);
-                  }}>
-              <MenuItem value="2nd-year">2nd-year</MenuItem>
-              <MenuItem value="3rd-year">3rd-year</MenuItem>
-              <MenuItem value="1st-year">1st-year</MenuItem>
-              <MenuItem value="4th-year">4th-year</MenuItem>
-          </Select> 
-          <Button type="submit" onClick={makePost}>Create Post</Button>
-      </div>
+                }}
+                noValidate
+                autoComplete="off"
+            >
 
-    </Box>
+                <div class="marg">
+                    <TextField
+                        style={{ width: 300 }}
+                        label="Post Title"
+                        id="title"
+                        variant="filled"
+                        error={errors.username !== ''}
+                        helperText={errors.username}
+                        required
+                        onChange={(e) => {
+                            setTitle(e.target.value);
+                        }} />
+                </div><div class="marg">
+                    <TextField
+                        style={{ width: 300 }}
+                        required
+                        label="Description"
+                        variant="filled"
+                        onChange={(e) => {
+                            setDescription(e.target.value);
+                        }}
+                        id="description"
+                        multiline
+                        rows={8}
+                        name="description" />
+                </div><div class="marg">
 
+                    <FormControl 
+                    ccontainer justifyContent="center">
+                        
+                    <InputLabel id="demo-simple-select-standard-label">Label</InputLabel>
+        
+                        <Select
+                            label={"Label"}
+                            value={label}
+                            variant="filled"
+                            style={{ width: 300 }}
+                            required
+                            labelId="categ"
+                            id="categ"
+                            onChange={(e) => {
+                                setLabel(e.target.value);
+                            }}
 
+                        >
+                            <MenuItem value="1st-year">1st-year</MenuItem>
+                            <MenuItem value="2nd-year">2nd-year</MenuItem>
+                            <MenuItem value="3rd-year">3rd-year</MenuItem>
+                            <MenuItem value="4th-year">4th-year</MenuItem>
+                        </Select>
+                    </FormControl>
+                
+                </div><div>
 
+                    <Button
+                        type="submit"
+                        onClick={makePost}
+                        variant="contained"> Post
+                    </Button>
+                </div>
+
+            </Box>
 
         </div>
 
     )
-  }
+}
